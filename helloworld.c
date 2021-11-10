ee2eade3afa3782b0bf702d454742dd912f896be
NBandemer <91515411+NBandemer@users.noreply.github.com>
2021-11-09 20:38:18 -0700

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 main() {
+  int number = 0; 
+  int* numberptr = &number;
   printf("Hello World!");
   printf("changes");
+  printf("Now we are gonna do some more interesting stuff")
+  scanf("Enter a number: %d, numberptr);
+  printf("Your number was: %d", number);
    return 0;
 }
