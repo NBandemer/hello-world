689efd8c10009e0c9d255d04a9a29304f591acf9
NBandemer <nbandeme@asu.edu>
2021-11-09 20:43:29 -0700

    fixes

--- helloworld.c
+++ helloworld.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-main() {
+int main() {
   int number = 0; 
   int* numberptr = &number;
   printf("Hello World!\n");
   printf("changes\n");
-  printf("Now we are gonna do some more interesting stuff\n")
+  printf("Now we are gonna do some more interesting stuff\n");
   scanf("Enter a number: %d", numberptr);
   printf("\nYour number was: %d", number);
    return 0;
